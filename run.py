2ac26744947fbe3ab7ab67ea978d9ff252db9a89
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2025-05-07 15:43:01 +0100

    update sales worksheet when valid data is inputted

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def get_sales_data():
         sales_data = data_str.split(",")
         if validate_data(sales_data):
             print("Data is valid")
-            break
+            return [int(value) for value in sales_data]
 
 def validate_data(values):
     """
@@ -47,4 +47,14 @@ def validate_data(values):
     
     return True
 
+def update_sales_worksheet(data):
+    """
+    Updates sales worksheet, add new row with the list data provided
+    """
+    print("updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("sales worksheet updated successfully\n")
+
 data = get_sales_data()
+update_sales_worksheet(data)
