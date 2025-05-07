bcbff1f53d0acaa3e468a493e3858c80dec6a849
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2025-05-07 14:14:55 +0100

    Ensure 6 values are entered in data

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     Get sales figures input from the user
@@ -21,7 +22,21 @@ def get_sales_data():
     print("eg: 56,2,34,22,67,9\n")
 
     data_str= input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Converts all strong values to intergers.
+    Raises ValueError if string cannot be converted
+    or if there aren't 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Numbers of values must equal 6. You provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
 get_sales_data()
-
